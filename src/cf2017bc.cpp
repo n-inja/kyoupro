88c45e58f1c720f890c69bf66a369e47ae643f02
n-inja <mccreepertakumi@gmail.com>
2018-09-18 15:02:17 +0900

    codefestival2017

--- src/cf2017bc.cpp
+++ src/cf2017bc.cpp
@@ -1,30 +1,59 @@
-#include<iostream>
-#include<fstream>
-#include<stdio.h>
-#include<string>
-#include<vector>
-#include<map>
-#include<math.h>
-#include<algorithm>
-#include<iomanip>
-#include<set>
-#include<utility>
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <set>
+#include <stdio.h>
+#include <string>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
+struct N {
+  int now, mode;
+};
+bool operator<(const N &l, const N &r) { return l.now < r.now; }
+
 int main() {
-  int n, m;
+  long long n, m;
   cin >> n >> m;
   vector<vector<int>> p(n);
   vector<set<int>> s(n);
-  for(int i = 0; i < m; i++) {
+  for (int i = 0; i < m; i++) {
     int a, b;
     cin >> a >> b;
-    a--; b--;
+    a--;
+    b--;
     p[a].push_back(b);
-    s[a].insert(b);
     p[b].push_back(a);
-    s[b].insert(a);
   }
+  set<int> o, e;
+  multiset<N> q;
+  vector<int> c(n);
+  q.insert(N{0, 1});
+  while (q.size() > 0) {
+    N now = *q.begin();
+    q.erase(q.begin());
+    if (c[now.now]) {
+      if (c[now.now] != now.mode) {
+        cout << n * (n - 1) / 2 - m << endl;
+        return 0;
+      }
+      continue;
+    }
+    c[now.now] = now.mode;
+    if (now.mode == 1)
+      o.insert(now.now);
+    else
+      e.insert(now.now);
+    for (int i = 0; i < p[now.now].size(); i++) {
+      q.insert(N{p[now.now][i], 2 - (now.mode - 1)});
+    }
+  }
+  long long os = o.size(), es = e.size();
+  cout << os * es - m << endl;
   return 0;
 }
--- src/cf2017bd.cpp
+++ src/cf2017bd.cpp
@@ -1,81 +1,87 @@
-#include<iostream>
-#include<fstream>
-#include<stdio.h>
-#include<string>
-#include<vector>
-#include<map>
-#include<math.h>
-#include<algorithm>
-#include<iomanip>
-#include<set>
-#include<utility>
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <set>
+#include <stdio.h>
+#include <string>
+#include <utility>
+#include <vector>
 
 using namespace std;
+
+vector<vector<long long>> dp(500001);
+long long solve(vector<int> &v) {
+  if (v.size() < 2)
+    return 0;
+  dp[0][0] = dp[1][0] = dp[0][1] = dp[1][1] = dp[0][2] = dp[1][2] = dp[0][3] =
+      dp[1][3] = 0;
+  for (int i = 1; i < v.size(); i++) {
+    dp[i + 1][0] = max(max(dp[i][0], dp[i][1]), max(dp[i][2], dp[i][3]));
+    if (v[i - 1] > 1) {
+      // right
+      dp[i + 1][2] =
+          max(max(dp[i][0] + v[i], dp[i][1] + v[i]), dp[i][3] + v[i]);
+      // left
+      dp[i + 1][1] = max(dp[i][0] + v[i - 1], dp[i][1] + v[i - 1] - 1);
+      if (v[i] > 1) {
+        dp[i + 1][3] =
+            max(max(dp[i][0] + v[i], dp[i][1] + v[i]), dp[i][3] + v[i]) - 1;
+      } else {
+        dp[i + 1][3] = 0;
+      }
+    } else {
+      dp[i + 1][2] = dp[i][0] + v[i];
+      dp[i + 1][1] = dp[i][0] + v[i - 1];
+      if (v[i] > 1) {
+        dp[i + 1][3] = dp[i][0] + v[i] - 1;
+      } else {
+        dp[i + 1][3] = 0;
+      }
+    }
+  }
+  return max(max(dp[v.size()][0], dp[v.size()][1]),
+             max(dp[v.size()][2], dp[v.size()][3]));
+}
 
 int main() {
   int n;
   string s;
   cin >> n >> s;
-  int ans = 0;
-  vector<int> sc;
-  int cnt = 0;
-  for(int i = 0; i < n; i++) {
-    if(s[i] == '1') {
-      cnt++;
-      continue;
+  for (int i = 0; i <= n; i++)
+    dp[i].resize(4);
+  s.push_back('0');
+  vector<pair<int, int>> v;
+  int now = '0', c = 1;
+  for (int i = 0; i < n + 1; i++) {
+    if (s[i] == now) {
+      c++;
+    } else {
+      v.push_back(make_pair(now, c));
+      c = 1;
     }
-    if(s[i] == '0') {
-      sc.push_back(cnt);
-      cnt = 0;
+    now = s[i];
+  }
+  vector<vector<int>> q;
+  vector<int> w;
+  for (int i = 1; i < v.size(); i += 2) {
+    if (v[i - 1].second == 1) {
+      w.push_back(v[i].second);
+    } else {
+      if (w.size() > 0)
+        q.push_back(w);
+      w.clear();
+      w.push_back(v[i].second);
     }
   }
-  sc.push_back(cnt);
-  sc.push_back(0);
-  int id = 0;
-  for(int i = 0; i < sc.size(); i++) {
-    if(sc[i] == 0) {
-      if(i - id < 2) {
-        id = i + 1;
-        continue;
-      }
-      int sum = 0;
-      int l = id, r = i - 1;
-    //  cout << l << " " << r << endl;
-      while(r - l > 1) {
-        if(sc[l + 1] > 1) {
-          sum += sc[l];
-          sc[l] = 0;
-          sc[l + 1] -= 1;
-          l++;
-          continue;
-        }
-        if(sc[r - 1] > 1) {
-          sum += sc[r];
-          sc[r] = 0;
-          sc[r - 1] -= 1;
-          r--;
-          continue;
-        }
-        if(sc[r] > sc[l]) {
-          sum += sc[r];
-          sc[r] = 0;
-          sc[r - 1] -= 1;
-          r--; r--;
-        } else {
-          sum += sc[l];
-          sc[l] = 0;
-          sc[l + 1] -= 1;
-          l++; l++;
-        }
-      }
-      if(r - l == 1) {
-        sum += max(sc[r], sc[l]);
-      }
-      ans += sum;
-      id = i + 1;
-    }
+  if (w.size() > 0)
+    q.push_back(w);
+  long long ans = 0;
+  for (int i = 0; i < q.size(); i++) {
+    ans += solve(q[i]);
   }
-  for(int i = 0; i < sc.size(); i++) cout << sc[i] << endl;
   cout << ans << endl;
   return 0;
 }
