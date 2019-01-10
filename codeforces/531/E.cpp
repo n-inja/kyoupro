c0d611b0d1a2c5c596ff01b8be526b4b1f1f58a5
n-inja <mccreepertakumi@gmail.com>
2019-01-10 13:55:35 +0900

    update graph, codeforces 531div3 E

--- codeforces/531/E.cpp
+++ codeforces/531/E.cpp
@@ -1,3 +1,16 @@
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <set>
+#include <stdio.h>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
 
 class G {
 public:
@@ -43,7 +56,7 @@ public:
     }
     int j = 0;
     for (int i = n - 1; i >= 0; i--) {
-      if (c[b[i]] > -1)
+      if (c[b[i]] >= 0)
         continue;
       dfs2(b[i], j++);
     }
@@ -60,3 +73,44 @@ public:
     }
   }
 };
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+  map<int, int> mp;
+  int c = 0;
+  for (int i = 0; i < n; i++) {
+    if (mp.find(a[i]) == mp.end()) {
+      mp[a[i]] = c++;
+    }
+  }
+
+  vector<vector<int>> p(c), pinv(c);
+  vector<set<int>> ps(c);
+  for (int i = 1; i < n; i++) {
+    if (a[i] == a[i - 1])
+      continue;
+    ps[mp[a[i - 1]]].insert(mp[a[i]]);
+  }
+  for (int i = 0; i < c; i++) {
+    for (auto &j : ps[i]) {
+      p[i].push_back(j);
+      pinv[j].push_back(i);
+    }
+  }
+
+  G g(c, p, pinv);
+  g.solve();
+  long long P = 998244353;
+  long long ans = 1;
+  for (int i = 1; i < g.sp.size(); i++) {
+    ans *= 2;
+    ans %= P;
+  }
+
+  cout << ans << endl;
+  return 0;
+}
