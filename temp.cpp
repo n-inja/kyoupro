026ce1cb94e7171a03eaeabcee4e9c9153134d06
n-inja <mccreepertakumi@gmail.com>
2019-01-12 20:08:46 +0900

    add template

---
+++ temp.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvl;
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  return 0;
+}
