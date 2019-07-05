d0de56096e2daa82ba7df71d97eef93cfbbf9926
n-inja <mccreepertakumi@gmail.com>
2019-07-05 11:22:55 +0900

    update lib

--- lib/euclid.cpp
+++ lib/euclid.cpp
@@ -1,16 +1,7 @@
-#include <algorithm>
-#include <fstream>
-#include <iomanip>
-#include <iostream>
-#include <map>
-#include <math.h>
-#include <set>
-#include <stdio.h>
-#include <string>
-#include <utility>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
+using ll = long long;
 
 long long gcd(long long a, long long b) {
   if (a % b == 0)
@@ -29,11 +20,11 @@ pair<long long, long long> exeuclid(long long a, long long b) {
 
 // v := [(modulo, reminder)]
 // moduloes should be coprime
-long long CRT(vector<pair<long long, long long>> v) {
+long long CRT(vector<pair<ll, ll>> v) {
   long long mod = 1;
   long long ret = 0;
   for (int i = 0; i < v.size(); i++) {
-    pair<long long, long long> xy = exeuclid(mod, v[i].first);
+    pair<ll, ll> xy = exeuclid(mod, v[i].first);
     long long M = mod * v[i].first;
     long long l = ((v[i].second * mod) % M) * xy.first;
     l %= M;
@@ -45,4 +36,168 @@ long long CRT(vector<pair<long long, long long>> v) {
     mod = M;
   }
   return ret;
+}
+
+ll mod_pow(ll e, ll x, ll p) {
+  ll r = 1;
+  e %= p;
+  while (x) {
+    if (x & 1) {
+      r *= e;
+      r %= p;
+    }
+    e *= e;
+    e %= p;
+    x >>= 1;
+  }
+  return r;
+}
+
+template<class T>
+T extgcd(T a, T b, T &x, T &y) {
+  for (T u = y = 1, v = x = 0; a;) {
+    T q = b / a;
+    swap(x -= q * u, u);
+    swap(y -= q * v, v);
+    swap(b -= q * a, a);
+  }
+  return b;
+}
+
+ll mod_inv(ll a, ll b) {
+  return (exeuclid(a, b).first % b + b) % b;
+}
+
+template<int mod, int primitive_root>
+class NTT {
+public:
+  int get_mod() const { return mod; }
+
+  void _ntt(vector<ll> &a, int sign) {
+    const int n = a.size();
+    assert((n ^ (n & -n)) == 0); //n = 2^k
+
+    const int g = 3; //g is primitive root of mod
+    int h = (int) mod_pow(g, (mod - 1) / n, mod); // h^n = 1
+    if (sign == -1) h = (int) mod_inv(h, mod); //h = h^-1 % mod
+
+    //bit reverse
+    int i = 0;
+    for (int j = 1; j < n - 1; ++j) {
+      for (int k = n >> 1; k > (i ^= k); k >>= 1);
+      if (j < i) swap(a[i], a[j]);
+    }
+
+    for (int m = 1; m < n; m *= 2) {
+      const int m2 = 2 * m;
+      const ll base = mod_pow(h, n / m2, mod);
+      ll w = 1;
+      for (int x = 0; x < m; x++) {
+        for (int s = x; s < n; s += m2) {
+          ll u = a[s];
+          ll d = a[s + m] * w % mod;
+          a[s] = u + d;
+          if (a[s] >= mod) a[s] -= mod;
+          a[s + m] = u - d;
+          if (a[s + m] < 0) a[s + m] += mod;
+        }
+        w = w * base % mod;
+      }
+    }
+
+    for (auto &x : a) if (x < 0) x += mod;
+  }
+
+  void ntt(vector<ll> &input) {
+    _ntt(input, 1);
+  }
+
+  void intt(vector<ll> &input) {
+    _ntt(input, -1);
+    const int n_inv = mod_inv(input.size(), mod);
+    for (auto &x : input) x = x * n_inv % mod;
+  }
+
+  // 畳み込み演算を行う
+  vector<ll> convolution(const vector<ll> &a, const vector<ll> &b) {
+    int ntt_size = 1;
+    while (ntt_size < a.size() + b.size()) ntt_size *= 2;
+
+    vector<ll> _a = a, _b = b;
+    _a.resize(ntt_size);
+    _b.resize(ntt_size);
+
+    ntt(_a);
+    ntt(_b);
+
+    for (int i = 0; i < ntt_size; i++) {
+      (_a[i] *= _b[i]) %= mod;
+    }
+
+    intt(_a);
+    return _a;
+  }
+};
+
+ll garner(vector<pair<ll, ll>> mr, int mod) {
+  mr.emplace_back(mod, 0);
+
+  vector<ll> coffs(mr.size(), 1);
+  vector<ll> constants(mr.size(), 0);
+  for (int i = 0; i < mr.size() - 1; i++) {
+    // coffs[i] * v + constants[i] == mr[i].second (mod mr[i].first) を解く
+    ll v = (mr[i].second - constants[i]) * mod_inv(coffs[i], mr[i].first) % mr[i].first;
+    if (v < 0) v += mr[i].first;
+
+    for (int j = i + 1; j < mr.size(); j++) {
+      (constants[j] += coffs[j] * v) %= mr[j].first;
+      (coffs[j] *= mr[i].first) %= mr[j].first;
+    }
+  }
+
+  return constants[mr.size() - 1];
+}
+
+typedef NTT<167772161, 3> NTT_1;
+typedef NTT<469762049, 3> NTT_2;
+typedef NTT<1224736769, 3> NTT_3;
+NTT_1 ntt1;
+NTT_2 ntt2;
+NTT_2 ntt3;
+// ref: https://math314.hateblo.jp/entry/2015/05/07/014908
+
+// NTT
+// c[i] = for j,k a[j] + b[k] if j + k == i mod P
+vector<ll> mod_conv(vector<ll> a, vector<ll> b, ll P) {
+  for (auto &i : a) i %= P;
+  for (auto &i : b) i %= P;
+  auto v1 = ntt1.convolution(a, b);
+  auto v2 = ntt2.convolution(a, b);
+  auto v3 = ntt3.convolution(a, b);
+  int n = v2.size();
+  vector<ll> ret(n);
+  for (int i = 0; i < n; i++) {
+    if (v1[i] != v2[i]) cerr << v1[i] << " " << v2[i] << endl;
+    ret[i] = garner(vector<pair<ll, ll>>(
+        {
+            make_pair(ntt1.get_mod(), v1[i]),
+            make_pair(ntt2.get_mod(), v2[i]),
+            make_pair(ntt3.get_mod(), v3[i])
+        }), P);
+  }
+  return ret;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<ll> a(n + 1), b(n + 1);
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i] >> b[i];
+  }
+  auto c = mod_conv(a, b, 1000000007);
+  for (int i = 1; i <= 2 * n; i++) {
+    cout << c[i] << endl;
+  }
+  return 0;
 }
--- lib/graph.cpp
+++ lib/graph.cpp
@@ -1,12 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
 
 class G {
 public:
-  vector <vector<int>> p, inv, sp, sinv;
+  vector<vector<int>> p, inv, sp, sinv;
   int n, m;
   vector<int> b, c, depth;
   vector<bool> a;
 
-  G(int nn, vector <vector<int>> &np, vector <vector<int>> &ninv) {
+  G(int nn, vector<vector<int>> &np, vector<vector<int>> &ninv) {
     n = nn;
     p = np;
     inv = ninv;
@@ -64,3 +67,105 @@ public:
     }
   }
 };
+
+struct SAT {
+  int n;
+  vector<pair<int, int>> conjs;
+
+  SAT() {
+    n = 0;
+  }
+
+  // a \/ b
+  void add_or(int a, int b) {
+    add_impl(-a, b);
+    add_impl(-b, a);
+  }
+
+  // a /\ b
+  void add_and(int a, int b) {
+    add_impl(a, a);
+    add_impl(b, b);
+  }
+
+  // a
+  void add_lit(int a) {
+    add_impl(a, a);
+  }
+
+  // a ^ b
+  void add_xor(int a, int b) {
+    add_or(a, b);
+    add_or(-a, -b);
+  }
+
+  // a => b
+  void add_impl(int a, int b) {
+    n = max(n, max(abs(a), abs(b)));
+    conjs.emplace_back(a, b);
+  }
+
+  // not(a \/ b)
+  void add_nor(int a, int b) {
+    add_and(-a, -b);
+  }
+
+  int f(int a) {
+    assert(a != 0);
+    if (a > 0) return a - 1;
+    else return n + abs(a) - 1;
+  }
+
+  bool solve() {
+    vector<vector<int>> p(2 * n), inv(2 * n);
+    for (auto &c : conjs) {
+      p[f(c.first)].push_back(f(c.second));
+      inv[f(c.second)].push_back(f(c.first));
+    }
+
+    G g(2 * n, p, inv);
+    g.solve();
+    bool ok = true;
+    for (int i = 0; i < n; i++) if (g.c[i] == g.c[i + n]) ok = false;
+    return ok;
+  }
+};
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<int> l(n), r(n);
+  for (int i = 0; i < n; i++) {
+    cin >> l[i] >> r[i];
+    r[i]++;
+  }
+  SAT solver;
+
+  // p_1, .., p_2n : 左右
+  vector<int> mp(2 * n);
+  for (int i = 0; i < n; i++) {
+    mp[2 * i] = i + 1;
+    mp[2 * i + 1] = -(i + 1);
+  }
+  vector<pair<int, int>> ps(2 * n);
+  for (int i = 0; i < n; i++) {
+    ps[i * 2] = make_pair(l[i], r[i]);
+    ps[i * 2 + 1] = make_pair(m - r[i], m - l[i]);
+  }
+  for (int i = 0; i < n * 2; i++) {
+    for (int j = i + 1; j < 2 * n; j++) {
+      if (i / 2 == j / 2) continue;
+      if (!(ps[i].second <= ps[j].first || ps[j].second <= ps[i].first)) {
+        solver.add_or(-mp[i], -mp[j]);
+      }
+    }
+  }
+
+  if (solver.solve()) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
+
+  return 0;
+}
