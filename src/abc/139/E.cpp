f3f41433f0009b517cd0ee2ffaa7a7baa013ace2
n-inja <mccreepertakumi@gmail.com>
2019-09-03 22:07:51 +0900

    abc139ef

--- src/abc/139/E.cpp
+++ src/abc/139/E.cpp
@@ -11,5 +11,49 @@ using vvl = vector<vector<ll>>;
 int main() {
   cin.tie(nullptr);
   ios::sync_with_stdio(false);
+  int n;
+  cin >> n;
+  vector<vector<int>> a(n, vector<int>(n - 1));
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n - 1; j++) {
+      cin >> a[i][j];
+      a[i][j]--;
+    }
+  }
+  int c = 0;
+  vector<int> ids(n);
+  set<int> look;
+  for (int i = 0; i < n; i++) look.insert(i);
+  for (int i = 0; i < n * n; i++) {
+    set<int> update;
+    for (auto it = look.begin(); it != look.end(); it++) {
+      int j = *it;
+      if (ids[j] >= n - 1) {
+        continue;
+      }
+      int to = a[j][ids[j]];
+      if (ids[to] >= n - 1) {
+        continue;
+      }
+      if (a[to][ids[to]] == j) {
+        update.insert(to);
+        update.insert(j);
+      }
+    }
+    if (update.empty()) {
+      cout << -1 << endl;
+      return 0;
+    }
+    look.clear();
+    for (auto &j : update) {
+      ids[j]++;
+      look.insert(j);
+      c++;
+    }
+    if (c == n * (n - 1)) {
+      cout << i + 1 << endl;
+      return 0;
+    }
+  }
   return 0;
 }
--- src/abc/139/F.cpp
+++ src/abc/139/F.cpp
@@ -7,9 +7,50 @@ using vi = vector<int>;
 using vll = vector<ll>;
 using vvi = vector<vector<int>>;
 using vvl = vector<vector<ll>>;
+double eps = 0.0000000000001;
 
 int main() {
   cin.tie(nullptr);
   ios::sync_with_stdio(false);
+  int n;
+  cin >> n;
+  vector<pair<long long, long long>> ps;
+  vector<pair<long long, long long>> qs;
+  for (int i = 0; i < n; i++) {
+    long long x, y;
+    cin >> x >> y;
+    if (x == 0 && y == 0) {
+    } else {
+      ps.emplace_back(x, y);
+      qs.emplace_back(x, y);
+      qs.emplace_back(-x, -y);
+      qs.emplace_back(-y, x);
+      qs.emplace_back(y, -x);
+    }
+  }
+  n = ps.size();
+  long long ans = 0.;
+  for (int i = 0; i < qs.size(); i++) {
+    long long x = 0, y = 0, dx1 = 0, dx2 = 0, dy1 = 0, dy2 = 0;
+    for (int j = 0; j < n; j++) {
+      if (qs[i].first * ps[j].first + qs[i].second * ps[j].second > 0) {
+        x += ps[j].first;
+        y += ps[j].second;
+      }
+      if (qs[i].first * ps[j].first + qs[i].second * ps[j].second == 0) {
+        if (qs[i].first * ps[j].second - qs[i].second * ps[j].first > 0) {
+          dx1 += ps[j].first;
+          dy1 += ps[j].second;
+        } else {
+          dx2 += ps[j].first;
+          dy2 += ps[j].second;
+        }
+      }
+    }
+    ans = max(ans, x * x + y * y);
+    ans = max(ans, (x + dx1) * (x + dx1) + (y + dy1) * (y + dy1));
+    ans = max(ans, (x + dx2) * (x + dx2) + (y + dy2) * (y + dy2));
+  }
+  printf("%.20lf\n", (double) sqrt((long double) ans));
   return 0;
 }
